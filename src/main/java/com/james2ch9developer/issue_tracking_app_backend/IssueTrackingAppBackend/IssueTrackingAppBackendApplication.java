e707177147c8f7e8090f6ed1768742bf14208b5c
James Hawley II <jameshawley2ch9@gmail.com>
2020-06-24 21:12:21 +0000

    Added comments.

--- src/main/java/com/james2ch9developer/issue_tracking_app_backend/IssueTrackingAppBackend/IssueTrackingAppBackendApplication.java
+++ src/main/java/com/james2ch9developer/issue_tracking_app_backend/IssueTrackingAppBackend/IssueTrackingAppBackendApplication.java
@@ -1,5 +1,12 @@
 package com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend;
 
+/* The package name is long, but basically my usual 'com.main' is eual to this ->
+com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend
+
+In a previous Spring-boot project a package named rest_controllers, so for this project it is ->
+com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend.rest_controllers
+*/
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
