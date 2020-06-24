fc35cd140e14ca90325f276e92d6dcf985ed104d
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-06-24 10:35:54 -0500

    Initial commit.

---
+++ src/main/java/com/james2ch9developer/issue_tracking_app_backend/IssueTrackingAppBackend/IssueTrackingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/james2ch9developer/issue_tracking_app_backend/IssueTrackingAppBackend/IssueTrackingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
