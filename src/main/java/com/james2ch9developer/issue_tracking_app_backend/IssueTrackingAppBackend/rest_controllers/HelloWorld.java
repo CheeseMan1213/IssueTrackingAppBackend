9d45d6de3bdf2eee1775b6ea977931725abd7734
James Hawley II <jameshawley2ch9@gmail.com>
2020-06-24 21:07:03 +0000

    Initial commit.

---
+++ src/main/java/com/james2ch9developer/issue_tracking_app_backend/IssueTrackingAppBackend/rest_controllers/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend.rest_controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class HelloWorld {
+    
+    @GetMapping(value = "/hello", produces = "text/plain")
+    public String HelloWorld() {
+        return "Hello World!";
+    }
+}
