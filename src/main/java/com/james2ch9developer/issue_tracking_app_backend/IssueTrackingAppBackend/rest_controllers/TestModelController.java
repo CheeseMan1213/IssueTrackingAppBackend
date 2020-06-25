1bb9a2c4aaddde8934260609f9dd6d63fed7cb22
James Hawley II <jameshawley2ch9@gmail.com>
2020-06-25 18:11:41 +0000

    Changed the RequestMapping annotation from '/testModel' to '/testApi'. Changed a get mapping annotation from '/getAllTestModels' to '/TestModels'. Added new get mapping annotation with method 'getTestModelById'.

--- src/main/java/com/james2ch9developer/issue_tracking_app_backend/IssueTrackingAppBackend/rest_controllers/TestModelController.java
+++ src/main/java/com/james2ch9developer/issue_tracking_app_backend/IssueTrackingAppBackend/rest_controllers/TestModelController.java
@@ -3,6 +3,7 @@ package com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBacken
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,24 @@ import com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend
 import com.james2ch9developer.issue_tracking_app_backend.IssueTrackingAppBackend.models.TestModel;
 
 @RestController
-@RequestMapping("/testModel")
+@RequestMapping("/testApi")
 public class TestModelController {
     
     @Autowired
     private TestModelRepository testModelRepository;
     
     // Get all TestModels.
-    @GetMapping(value = "/getAllTestModels", produces = "application/json")
+    @GetMapping(value = "/TestModels", produces = "application/json")
     public List<TestModel> getAllTestModels() {
         return this.testModelRepository.findAll();
     }
+    
+    // Get one TestModel by id.
+    @GetMapping("/TestModels/{id}")
+	public ResponseEntity<TestModel> getTestModelById(@PathVariable(value = "id") Long TestModelId)
+	    throws ResourceNotFoundException {
+	        TestModel testModel = testModelRepository.findById(TestModelId)
+	            .orElseThrow(() -> new ResourceNotFoundException("TestModel not found for this id :: " + TestModelId));
+	        return ResponseEntity.ok().body(testModel);
+	    }
 }
