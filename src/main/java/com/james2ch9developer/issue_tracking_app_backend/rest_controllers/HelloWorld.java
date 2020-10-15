fe042cfe525ede9eb941b8a2bed066c08cbcb278
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-10-15 14:52:06 -0500

    In order to satisfy the linter PMD, I added the @SuppressWarnings annotation to the top of the class.

--- src/main/java/com/james2ch9developer/issue_tracking_app_backend/rest_controllers/HelloWorld.java
+++ src/main/java/com/james2ch9developer/issue_tracking_app_backend/rest_controllers/HelloWorld.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+// I am suppressing this warning because this is a Spring-boot rest controller.
+// This class does not need a constructor, and I do not need PMD telling me otherwise.
+@SuppressWarnings("PMD.AtLeastOneConstructor")
 public class HelloWorld {
     
     @GetMapping(value = "/hello", produces = "text/plain")
